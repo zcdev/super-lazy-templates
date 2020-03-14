c5039266a78e984a906166f4888b7889dbd9e3a4
ZCDEV <zoechangdev@gmail.com>
2020-03-14 01:50:25 -0700

    Upload in one lazy stike

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# super-lazy-templates
+# Super Lazy Templates
+<hr>
+<p>
+Just getting SUPER LAZY! This is the more sophisticated version of the cowgirl LAZY Templates by using Slim + Stylus + Gulp.js.
+</p>
+<p>Slim + Stylus + Gulp = Super</p>
+http://slim-lang.com<br>
+http://stylus-lang.com<br>
+http://gulp.js
+
+How to be SUPER LAZY in 2 steps:
+1. Installation: `npm install`
+2. Run: `gulp super-lazy`
+
+PS. You might have to `sudo gem install slim` to get slim a bit before you can run fast ;-)))
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var slim = require("gulp-slim");
+var stylus = require('gulp-stylus');
+var browserSync = require('browser-sync').create();
+
+gulp.task('slim', function(){
+  return gulp.src('./src/*.slim')
+    .pipe(slim({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('stylus', function() {
+  return gulp.src('./src/*.styl')
+    .pipe(stylus({
+      'include css': true
+    }))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+ 
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    },
+  })
+});
+
+gulp.task('watch', gulp.series('browserSync', 'slim', 'stylus'), function(){
+  gulp.watch('./src/*.slim', ['slim']); 
+  gulp.watch('./src/*.stylus', ['styl']);  
+});
+
+gulp.task('super-lazy', gulp.series('slim', 'stylus'), function (){
+  console.log('Lazily building files...');
+})
+
